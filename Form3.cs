7f3666badc4d333a38b2f6f6eefab78430653221
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2026-04-16 01:33:44 +0700

    implementasi logika muat data lama dan penyimpanan data sayur

--- Form3.cs
+++ Form3.cs
@@ -17,6 +17,75 @@ namespace SistemManajemenDistributorSayur
         string idTerpilih = "";
 
         
-       
+        public FormTambahSayur(string id)
+        {
+            InitializeComponent();
+            idTerpilih = id;
+
+            if (idTerpilih != "")
+            {
+                this.Text = "Edit Data Sayur";
+                MuatDataLama();
+            }
+            else
+            {
+                this.Text = "Tambah Sayur Baru";
+            }
+        }
+
+        private void MuatDataLama()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+               
+                string query = "SELECT * FROM Sayur WHERE SayurID=@id";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", idTerpilih);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    txtNama.Text = dr["NamaSayur"].ToString();
+                    txtStok.Text = dr["Stok"].ToString();
+                    txtHarga.Text = dr["HargaJual"].ToString();
+                }
+            }
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+           
+            if (string.IsNullOrEmpty(txtNama.Text) || string.IsNullOrEmpty(txtStok.Text) || string.IsNullOrEmpty(txtHarga.Text))
+            {
+                MessageBox.Show("Semua data (Nama, Stok, Harga) harus diisi!", "Peringatan");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection("Data Source=LAPTOP-V3CL2RKG\\BEBEB;Initial Catalog=DBDistributorsayur;Integrated Security=True"))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Sayur (NamaSayur, Kategori, Stok, HargaJual) VALUES (@nama, @kat, @stok, @harga)";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@nama", txtNama.Text);
+                    cmd.Parameters.AddWithValue("@kat", "Umum"); 
+                    cmd.Parameters.AddWithValue("@stok", int.Parse(txtStok.Text));
+                    cmd.Parameters.AddWithValue("@harga", double.Parse(txtHarga.Text));
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data sayur berhasil disimpan!", "Sukses");
+                    this.Close(); 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal simpan: " + ex.Message);
+                }
+            }
+        }
     }
 }
