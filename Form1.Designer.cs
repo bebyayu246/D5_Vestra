c22ddeedbac559fa3c6deba0076344dfa4ef3d8f
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2026-04-16 01:24:37 +0700

    Metode InitializeComponent() adalah blok kode otomatis yang berfungsi untuk membangun tampilan visual formulir saat aplikasi pertama kali dijalankan.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -18,7 +18,24 @@
         #region Windows Form Designer generated code
 
         
-        
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+           
     }
 }
 
